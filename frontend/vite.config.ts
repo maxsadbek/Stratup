0cce3efff4fbddaea2814c6778c45350cd5c6462
maxsadbek <masadanb@gmail.com>
2026-05-27 08:07:59 +0500

    add

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import react from "@vitejs/plugin-react";
+// @ts-ignore
+import path, { dirname } from "path";
+// @ts-ignore
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
+
+// @ts-ignore
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
     port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        target: "http://localhost:3000",
         changeOrigin: true,
       },
     },
